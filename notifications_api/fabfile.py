19fe9e86b0c65feb1d5da12f5937ff3a762ce713
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-26 19:39:01 -0400

    Fix deploy script

--- notifications_api/fabfile.py
+++ notifications_api/fabfile.py
@@ -1,11 +1,11 @@
-from fabric.api import cd, run
+from fabric.api import cd, run, prefix
 
 
 def deploy():
     with cd("/usr/local/www/notifications"):
         run("git pull")
         with cd("notifications_api"):
-            run("source /usr/local/virtualenvs/notifications/bin/activate")
-            run("./manage.py migrate")
-            run("./manage.py collectstatic --noinput")
-            run("sudo supervisorctl restart notifications")
+            with prefix("source /usr/local/virtualenvs/notifications/bin/activate && export PRODUCTION=TRUE"):
+                run("./manage.py migrate")
+                run("./manage.py collectstatic --noinput")
+            run("supervisorctl restart notifications")
